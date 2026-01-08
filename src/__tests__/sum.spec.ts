a0fc48e393ad393df8a69aa968019a7a49f78ba1
cde123456781 <bjc199@uclive.ac.nz>
2026-01-08 17:57:00 +1300

    Relocated files

---
+++ src/__tests__/sum.spec.ts
@@ -0,0 +1,7 @@
+import { describe, expect, it } from 'vitest';
+
+describe('sum function', () => {
+    it('should add two positive numbers correctly', () => {
+        expect(1+2).toBe(3);
+    });
+});
