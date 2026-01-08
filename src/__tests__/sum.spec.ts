3666069bf884ad2327d94ad3c47337ef80594e55
cde123456781 <bjc199@uclive.ac.nz>
2026-01-08 18:17:31 +1300

    Add prettier

--- src/__tests__/sum.spec.ts
+++ src/__tests__/sum.spec.ts
@@ -1,7 +1,7 @@
-import { describe, expect, it } from 'vitest';
+import { describe, expect, it } from "vitest";
 
-describe('sum function', () => {
-    it('should add two positive numbers correctly', () => {
-        expect(1+2).toBe(3);
-    });
+describe("sum function", () => {
+  it("should add two positive numbers correctly", () => {
+    expect(1 + 2).toBe(3);
+  });
 });
